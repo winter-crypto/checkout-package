2532194026e9ea8de96b58d01dff3e0940b42f5c
Evan Fisher <57010682+Evan-Fisher@users.noreply.github.com>
2022-11-05 14:17:10 -0700

    deprecated goerli url, added in color scheme in css to flash on load when using next js, added in core-js peer-dependency to get rid of core-js error

--- src/components/WinterCheckout.js
+++ src/components/WinterCheckout.js
@@ -93,8 +93,6 @@ const WinterCheckout = ({
 
     const url = production
       ? "https://checkout.usewinter.com/?" + queryString
-      : testnet === "goerli"
-      ? "https://goerli-sandbox-checkout.onrender.com/?" + queryString
       : "https://sandbox-winter-checkout.onrender.com/?" + queryString;
     setProjectUrl(url);
   }, [
@@ -116,7 +114,7 @@ const WinterCheckout = ({
   return showModal ? (
     <div
       dangerouslySetInnerHTML={{
-        __html: `<iframe id="winter-checkout" src="${projectUrl}" style="position: fixed; top: 0px; bottom: 0px; right: 0px; width: 100%; border: none; margin: 0px; padding: 0px; overflow: hidden; z-index: 999999; height: 100%;" />`,
+        __html: `<iframe id="winter-checkout" src="${projectUrl}" style="color-scheme: light; position: fixed; top: 0px; bottom: 0px; right: 0px; width: 100%; border: none; margin: 0px; padding: 0px; overflow: hidden; z-index: 999999; height: 100%;" />`,
       }}
     />
   ) : (
